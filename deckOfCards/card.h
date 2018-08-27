01511d5f0ac6b238adaaac679a04c7002075d857
Imaad Mohammed <imaadmohammed@gmail.com>
2018-08-26 17:06:00 -0700

    changes

--- deckOfCards/card.h
+++ deckOfCards/card.h
@@ -6,22 +6,71 @@
 //makefile
 
 #include <iomanip>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 class Card
 {
-private:
+  private:
 	string suit;
 	char rank;
 
-public:
+  public:
+	Card()
+	{
+		suit = "Spade";
+		rank = '2';
+	}
 
-	Card();
-	Card(string s, char r);
+	Card(string s, char r)
+	{
+		suit = s;
+		rank = r;
+	}
 
-	void createDeck(Card cards[]);
-	void printDeck(Card cards[]);
-	void shuffleDeck(Card cards[]);
+	void createDeck(Card cards[52])
+	{
+		int x = 0;
+		string suit[] = {"Spades", "Clubs", "Hearts", "Diamonds"};
+		char rank[] = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'};
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 13; j++)
+			{
+				cards[x] = Card(suit[i], rank[j]);
+				x++;
+			}
+		}
+	}
 
+	void printDeck(Card cards[52])
+	{
+		for (int i = 0; i < 52; i++)
+			cout << cards[i].rank << "of " << cards[i].suit << endl;
+	}
+
+	void shuffleDeck(Card cards[52])
+	{
+		Card left[26];
+		Card right[26];
+		int o = 0;
+		for (int i = 0; i < 26; i++)
+		{
+			left[i] = cards[i];
+		}
+		for (int x = 27; x < 52; x++)
+		{
+			right[o] = cards[x];
+			o++;
+		}
+
+		for (int j = 0; j < 52; j++)
+		{
+			cards[j] = left[j];
+			if (j < 51)
+				cards[j + 1] = right[j];
+		}
+	}
 };
--- deckOfCards/hw02.cpp
+++ deckOfCards/hw02.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "card.h"
-#include "card.cpp"
 
 using namespace std;
 
@@ -12,7 +11,7 @@ int main()
 {
     Card deck[52];
     deck[52].createDeck(deck);
-    
+
     cout << "Printing deck..." << endl;
     deck[52].printDeck(deck);
 
