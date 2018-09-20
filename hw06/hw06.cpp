e8beea2bc2edf19aae1f22e60cbdb2ec606f8c6c
Imaad Mohammed <imaadmohammed@gmail.com>
2018-09-20 10:37:38 -0700

    hw06

--- hw06/hw06.cpp
+++ hw06/hw06.cpp
@@ -3,15 +3,37 @@
 
 using namespace std;
 
-void recursiveString(string r);
+string recursiveString(string &s, int l, int r);
 
 int main()
 {
-    string abcdefghijklmnopqrstuvwxyz;
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    string a2 = "abcdefghijklmnopqrstuvwxyz";
+    string a3 = "abcdefghijklmnopqrstuvwxyz";
+    string first = recursiveString(alphabet,11,18);
+    string second = recursiveString(a2,4,22);
+    string third = recursiveString(a3,0,25);
+    cout << "First swap: " << first << endl;
+    cout << "Second swap: " << second << endl;
+    cout << "Third swap: " << third << endl;
 
 }
 
-void recursiveString(string r)
+string recursiveString(string &s, int l, int r)
 {
-    
+
+    if (l >= r)
+        return "Could not reverse";
+    else
+    {
+    char temp = s[l];
+    s[l] = s[r];
+    s[r] = temp;
+
+    recursiveString(s,l+1,r-1);
+    }
+
+    return s;
+
+
 }
